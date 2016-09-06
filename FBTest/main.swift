b1a108668538d33cfd64b40b06e6cff4ceb90b2c
Maxim Zaks <maxim.zaks@gmail.com>
2016-09-06 10:41:36 +0200

    Initial Commit

---
+++ FBTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FBTest
+//
+//  Created by Maxim Zaks on 06.09.16.
+//  Copyright © 2016 maxim.zaks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
